c712863f06e5c8b6c2da5e49f42dff335b91474b
Mansour.vn <mansour.vn86@gmail.com>
2024-09-17 16:25:34 +0330

    feat: Part4 add joint table relations

---
+++ PokemonReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonID { get; set; }
+        public string CategoryID { get; set;}
+        public Pokemon Pokemon { get; set; }
+    }
+}
--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -6,6 +6,6 @@
         public string Title { get; set; }
         public string Text { get; set; }
         public Reviewer Reviewer { get; set; }
-        public pokemon pokemon { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
